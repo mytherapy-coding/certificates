6755089d7baf726e37d37dc02ec0cca31ea3b41f
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-02-10 19:38:28 -0500

    subclass

--- PCAP/pcap_practice.py
+++ PCAP/pcap_practice.py
@@ -270,3 +270,30 @@ timer.prev_second()
 print(timer)
 
 print()
+class SampleClass:
+    def __init__(self, val):
+        self.val = val
+
+
+object_1 = SampleClass(0)
+object_2 = SampleClass(2)
+object_3 = object_1
+object_3.val += 1
+
+print(object_1 is object_2)
+print(object_2 is object_3)
+print(object_3 is object_1)
+print(object_1.val, object_2.val, object_3.val)
+
+string_1 = "Mary had a little "
+string_2 = "Mary had a little lamb"
+string_1 += "lamb"
+string_3 = "Mary had a little lamb"
+
+
+print(string_1 == string_2, string_1 is string_2, string_2 is string_3)
+print(id(string_1), id(string_2), id(string_3))
+
+
+print()
+
