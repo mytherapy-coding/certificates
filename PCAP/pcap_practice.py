1434e425795d8ae0bd95854f302476862b4be4c9
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2025-02-26 23:10:29 -0500

    mod

--- PCAP/pcap_practice.py
+++ PCAP/pcap_practice.py
@@ -510,3 +510,12 @@ print(trunc(x), trunc(y))
 print(trunc(-x), trunc(-y))
 
 print()
+
+from random import randrange, randint
+
+print(randrange(1), end=' ')
+print(randrange(0, 1), end=' ')
+print(randrange(0, 1, 1), end=' ')
+print(randint(0, 1))
+
+print()
